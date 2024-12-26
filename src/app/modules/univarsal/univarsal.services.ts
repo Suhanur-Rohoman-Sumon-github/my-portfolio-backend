502e90b167218f67cc59e198b73edbaafa228874
Suhnaur-Rohoman-Sumon-github <sumonsuhanurrohoman@gmail.com>
2024-12-26 16:15:14 +0600

    all services added

--- src/app/modules/univarsal/univarsal.services.ts
+++ src/app/modules/univarsal/univarsal.services.ts
@@ -1,11 +1,25 @@
 import { TProjects } from "./univarsal.interface";
-import { projectModel } from "./univarsal.model";
+import { blogModel, ExperienceModel, projectModel } from "./univarsal.model";
 
 
 const createProjectInDb = async (payload: TProjects) => {
   const newUser = payload;
   
   const result = await projectModel.create(newUser);
+
+  return result;
+};
+const createBlogInDb = async (payload: TProjects) => {
+  const newUser = payload;
+  
+  const result = await blogModel.create(newUser);
+
+  return result;
+};
+const createExperienceInDb = async (payload: TProjects) => {
+  const newUser = payload;
+  
+  const result = await ExperienceModel.create(newUser);
 
   return result;
 };
@@ -18,7 +32,45 @@ const getAllProjectFromDb = async () => {
   return result;
 };
 
+const getAllBlogsFromDb = async () => {
+  
+  
+  const result = await blogModel.find();
+
+  return result;
+};
+
+const getAllExperienceFromDb = async () => {
+  
+  
+  const result = await ExperienceModel.find();
+
+  return result;
+};
+const getSingleBlogsFromDb = async (blogId:string) => {
+  
+  
+  const result = await ExperienceModel.findOne({id:blogId});
+
+  return result;
+};
+const getSingleProjectsFromDb = async (projectId:string) => {
+  
+  
+  const result = await ExperienceModel.findOne({id:projectId});
+
+  return result;
+};
+
+
+
 export const ProjectServices = {
     createProjectInDb,
-    getAllProjectFromDb
+    getAllProjectFromDb,
+    createBlogInDb,
+    createExperienceInDb,
+    getAllExperienceFromDb,
+    getAllBlogsFromDb,
+    getSingleBlogsFromDb,
+    getSingleProjectsFromDb
 }
--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,14 @@
 import mongoose from 'mongoose';
 import app from './app';
+
+import config from './app/config';
 import { Server } from 'http';
-import config from './config';
+
 let server: Server;
 async function main() {
   try {
     await mongoose.connect(config.database_url as string);
-    app.listen(config.port, () => {
+    server = app.listen(config.port, () => {
       console.log(`app is  listening on port ${config.port}`);
     });
   } catch (error) {
